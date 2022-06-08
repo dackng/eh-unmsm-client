d09d79d0a37ea4d689f3c34b474ef11636e75926
Diego Cayo Alcos <dackng@gmail.com>
2022-06-08 15:51:55 -0500

    deleting node_modules

--- node_modules/angular2-logger/dist/es6/core.js
+++
@@ -1,4 +0,0 @@
-export * from "./app/core/level";
-export * from "./app/core/logger";
-export * from "./app/core/providers";
-//# sourceMappingURL=core.js.map
--- node_modules/libphonenumber-js/autoupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export PATH=/usr/local/bin:$PATH
-
-# Enter this script's folder
-cd "${0%/*}"
-
-npm run metadata:update
-npm run metadata:pull-request
